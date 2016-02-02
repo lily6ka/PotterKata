2073ff09f91cd4cba7e8609c62c6242ae676be25
olga kornienko <o.kornienko@godeltech.com>
2016-02-02 13:09:07 +0300

    added tests for PriceCalculationService

---
+++ BookStore.Tests/PriceCalculationServiceTests.cs
@@ -0,0 +1,96 @@
+﻿using BookStore.Interfaces;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace BookStore.Tests
+{
+
+    [TestFixture]
+    public class PriceCalculationServiceTests
+    {
+        private IPriceCalculationService _calculatorService;
+
+        private IOrder _order;
+
+        [Test]
+        public void CalculateOrderPrice_should_calculate_price_correctly_of_mixed_order_series()
+        {
+            decimal actual = _calculatorService.CalculateOrderPrice(_order);
+
+            actual.Should().Be(65.6m);
+        }
+
+        [Test]
+        public void CalculateOrderPrice_should_return_0_if_order_is_empty()
+        {
+            decimal actual = _calculatorService.CalculateOrderPrice(_order);
+
+            actual.Should().Be(0m);
+        }
+
+        [Test]
+        public void CalculateOrderPrice_should_return_0_if_order_is_null()
+        {
+            decimal actual = _calculatorService.CalculateOrderPrice(null);
+
+            actual.Should().Be(0m);
+        }
+
+        [Test]
+        public void CalculateOrderPrice_should_calculate_no_discount_if_order_has_no_different_series()
+        {
+            decimal actual = _calculatorService.CalculateOrderPrice(_order);
+
+            actual.Should().Be(16m);
+        }
+
+        [Test]
+        public void CalculateOrderPrice_should_calculate_5_rate_discount_if_order_has_two_different_series()
+        {
+            decimal actual = _calculatorService.CalculateOrderPrice(_order);
+
+            actual.Should().Be(15.2m);
+        }
+
+        [Test]
+        public void CalculateOrderPrice_should_calculate_10_rate_discount_if_order_has_three_different_series()
+        {
+
+            decimal actual = _calculatorService.CalculateOrderPrice(_order);
+
+            actual.Should().Be(21.6m);
+        }
+
+        [Test]
+        public void CalculateOrderPrice_should_calculate_15_rate_discount_if_order_has_four_different_series()
+        {
+            decimal actual = _calculatorService.CalculateOrderPrice(_order);
+
+            actual.Should().Be(27.2m);
+        }
+
+        [Test]
+        public void CalculateOrderPrice_should_calculate_25_rate_discount_if_order_has_five_different_series()
+        {
+            decimal actual = _calculatorService.CalculateOrderPrice(_order);
+
+            actual.Should().Be(30m);
+        }
+
+        [Test]
+        public void CalculateOrderPrice_should_calculate_30_rate_discount_if_order_has_six_different_series()
+        {
+            decimal actual = _calculatorService.CalculateOrderPrice(_order);
+
+            actual.Should().Be(33.6m);
+        }
+
+        [Test]
+        public void CalculateOrderPrice_should_calculate_35_rate_discount_if_order_has_seven_different_series()
+        {
+            decimal actual = _calculatorService.CalculateOrderPrice(_order);
+
+            actual.Should().Be(36.4m);
+        }
+    }
+}
